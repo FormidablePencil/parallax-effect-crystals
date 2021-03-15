a7c48b0a534453754cd3df1259a9b2eff4aa5a5e
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-15 16:34:28 -0700

    modded alittle

--- src/CrystalParallax.tsx
+++ src/CrystalParallax.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react'
 import CrystalParallaxEffect from './exports/CrystalParallaxEffect'
-import { useParallaxPropertiesT } from './hooks/useParallaxProperties';
-import { crystalParallaxT } from '.';
-import { CrystalParallaxContext } from './CrystalParallaxProvider';
-import ParallaxGui from './components/ParallaxGui';
+import { useParallaxPropertiesT } from './hooks/useParallaxProperties'
+import { crystalParallaxT } from '.'
+import { CrystalParallaxContext } from './CrystalParallaxProvider'
+import ParallaxGui from './components/ParallaxGui'
 
 interface CrystalParallaxCompT {
   pulledRawCrystalData: crystalParallaxT
@@ -12,32 +12,41 @@ interface CrystalParallaxCompT {
 }
 const CrystalParallax = (props: CrystalParallaxCompT) => {
   const { withGui } = props
-  
+
   const {
-    dispatchCrystalData, crystalData,
-    crystalIndex, setCrystalIndex,
-    crystalSelectionDistinction, setCrystalSelectionDistinction,
-    selectedForModeColors, setSelectedForModeColors,
-    modMenuFixed, setModMenuFixed,
+    dispatchCrystalData,
+    crystalData,
+    crystalIndex,
+    setCrystalIndex,
+    crystalSelectionDistinction,
+    setCrystalSelectionDistinction,
+    selectedForModeColors,
+    setSelectedForModeColors,
+    modMenuFixed,
+    setModMenuFixed,
     addSpecificCrystal,
-    deleteCrystal,
+    deleteCrystal
   }: useParallaxPropertiesT = useContext<any>(CrystalParallaxContext)
 
   return (
     <CrystalParallaxEffect {...props}>
-      {withGui &&
+      {withGui && (
         <ParallaxGui
           deleteCrystal={deleteCrystal}
           addSpecificCrystal={addSpecificCrystal}
-          modMenuFixed={modMenuFixed} setModMenuFixed={setModMenuFixed}
-          selectedForModeColors={selectedForModeColors} setSelectedForModeColors={setSelectedForModeColors}
-          crystalSelectionDistinction={crystalSelectionDistinction} setCrystalSelectionDistinction={setCrystalSelectionDistinction}
-          crystalIndex={crystalIndex} setCrystalIndex={setCrystalIndex}
-          crystalData={crystalData} dispatchCrystalData={dispatchCrystalData}
+          modMenuFixed={modMenuFixed}
+          setModMenuFixed={setModMenuFixed}
+          selectedForModeColors={selectedForModeColors}
+          setSelectedForModeColors={setSelectedForModeColors}
+          crystalSelectionDistinction={crystalSelectionDistinction}
+          setCrystalSelectionDistinction={setCrystalSelectionDistinction}
+          crystalIndex={crystalIndex}
+          setCrystalIndex={setCrystalIndex}
+          crystalData={crystalData}
+          dispatchCrystalData={dispatchCrystalData}
         />
-      }
+      )}
     </CrystalParallaxEffect>
-
   )
 }
 export default CrystalParallax
